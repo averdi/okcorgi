2ff61602530e47c06cf1363f94d51627020a95cd
Antonia Verdi <averdi@gm.slc.edu>
2016-04-01 13:41:38 -0500

    small changes

--- models/corgi.js
+++ models/corgi.js
@@ -5,11 +5,22 @@ var corgiSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   pictureURL: String,
+  liked: Boolean,
   created_at: Date,
   updated_at: Date
 });
 
+
+// js for getting random dogs ea time goes here.
+
+
+
 var Corgi = mongoose.model('Corgi', corgiSchema);
 
 // Make this available to our other files
+
+
 module.exports = Corgi;
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'OK Corgi' });
+});
+
+// router.post js to put liked dog into an array
+router.post('/corgis/:id', function(req, res, next){
+
+// both forms submit to this. need if/else for like or dislike forms.
+// will need mongodb queries, findbyidandupdate
 });
 
 module.exports = router;
