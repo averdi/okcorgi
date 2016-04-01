0a9aa06244acd5b7873d1238605f390e387903ae
Antonia Verdi <averdi@gm.slc.edu>
2016-04-01 14:11:49 -0500

    trying to pull data from db to site

--- routes/corgis.js
+++ routes/corgis.js
@@ -10,4 +10,14 @@ router.get('/', function(req, res, next) {
   res.send('whatever was automatically here before i changed it');
 });
 
+
+
+router.get('/', function(req, res, next) {
+  Corgi.find({}, function(err, corgis) {
+    res.render('index', {
+      title: 'Corgis you Like',
+      corgis: corgis
+    });
+  });
+
 module.exports = router;
